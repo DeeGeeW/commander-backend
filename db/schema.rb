69e3ba45472b0abaf5790bcbf5bdecec6367ccb0
DeeGeeWest <DeeGeeWest@gmail.com>
2022-05-23 13:48:20 -0400

    added creatures to deck

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_21_190215) do
     t.boolean "is_exile"
     t.boolean "tapped"
     t.boolean "is_land"
+    t.boolean "is_creature"
     t.integer "deck_num"
     t.string "counters"
     t.datetime "created_at", null: false
