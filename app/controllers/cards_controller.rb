f9831a967c270191bbd89e07dfec4385aa3e626e
DeeGeeWest <DeeGeeWest@gmail.com>
2022-05-23 18:40:11 -0400

    added crud routes http req for cards and half decks

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,36 @@
 class CardsController < ApplicationController
+  def index
+    cards = Card.all
+    render json: cards.as_json
+  end
+
+  def create
+    card = Card.new(
+      name: params[:name],
+      width: params[:width],
+      height: params[:height]
+    )
+    card.save
+    render json: card.as_json
+  end
+
+  def show
+    card = Card.find_by(id: params[:id])
+    render json: card.as_json
+  end
+
+  def update
+    card = Card.find_by(id: params[:id])
+    card.name = params[:name] || card.name
+    card.width = params[:width] || card.width
+    card.height = params[:height] || card.height
+    card.save
+    render json: card.as_json
+  end
+
+  def destroy
+    card = Card.find_by(id: params[:id])
+    card.destroy
+    render json: {message: "card successfully destroyed."}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  ## CARD DB
+  get "/cards" => "cards#index"
+  post "/cards" => "cards#create"
+  get "/cards/:id" => "cards#show"
+  patch "/cards/:id" => "cards#update"
+  delete "/cards/:id" => "cards#destroy"
+  
+  ## DECK DB
+  get "/decks" => "decks#index"
+  post "/decks" => "decks#create"
+  get "/decks/:id" => "decks#show"
+  patch "/decks/:id" => "decks#update"
+  delete "/decks/:id" => "decks#destroy"
+  
 end
