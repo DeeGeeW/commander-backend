acd999f24661ebbb44d3e6247a090a16d3bf67f0
DeeGeeWest <DeeGeeWest@gmail.com>
2022-08-27 00:08:05 -0400

    got all draw and renders to compplete

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,21 +1,15 @@
 class Deck < ApplicationRecord
-
-
   def cards
     Card.where(deck_id: id, is_active: false)
   end
 
-  
-    
-  
   # def draw
-    
+
   # end
-  
+
   # def hand
   #   Card.where(deck_id: id, is_hand: true, is_active: true)
   # end
-
 
   # def hand
   #   Card.where(is_hand: true, is_active: true)
